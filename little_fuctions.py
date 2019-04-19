2de16d4ce54f1699c85c66544669a052bc9748fe
R31N3 <dfinogeev3@yandex.ru>
2019-04-19 20:59:35 +0300

    It just works! Remastered! 2 2 Now featuring leader board and Dante from DMC Series! 2.0151221345068 Pre-Alpha One-day patch Final version GOTY Edition (please stop)

--- little_fuctions.py
+++ little_fuctions.py
@@ -14,7 +14,7 @@ def get_suggests(user_storage):
     return suggests, user_storage
 
 
-def IDontUnderstand(response, user_storage, answer):
+def message_error(response, user_storage, answer):
     import random
     message = random.choice(answer)
     response.set_text(message)
@@ -34,11 +34,30 @@ def read_answers_data(name):
 aliceAnswers = read_answers_data("data/answers_dict_example")
 
 
+def choice_wrd(last_char, used_words):
+    if type(used_words) is not list:
+        used_words = []
+    from random import choice
+    user_word = choice(read_answers_data("data/words")[last_char])
+    while user_word in used_words:
+        user_word = choice(read_answers_data("data/words")[last_char])
+    return user_word
+
+
+# Ну вот эта функция всем функциям функция, ага. Замена постоянному формированию ответа, ага, экономит 4 строчки!!
+def message_return(response, user_storage, message):  # ща будет магия
+    response.set_text(message)
+    response.set_tts(message)
+    buttons, user_storage = get_suggests(user_storage)
+    response.set_buttons(buttons)
+    return response, user_storage
+
+
 def check_wrd(wrd):
     import requests
     API_CODE = 'dict.1.1.20190331T101514Z.9cbf4535b1122019.dbd3fb8c0fded55cd45d1f44459bbfda21d8e82a'
-    BASIC_REQUEST = '''https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={API_CODE}&lang=TRANS&text=TEXTWORD'''.format(
-        API_CODE=API_CODE)
+    BASIC_REQUEST = '''https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={API_CODE}
+    &lang=TRANS&text=TEXTWORD'''.format(API_CODE=API_CODE)
     ERRORS = {200: 'Запрос выполнен',
               401: 'Ключ API невалиден',
               402: 'Ключ API заблокирован',
--- main_function.py
+++ main_function.py
@@ -2,34 +2,12 @@
 from __future__ import unicode_literals
 from little_fuctions import *
 
-aliceAnswers = read_answers_data("data/answers_dict_example")
-
-
-def choice_wrd(last_char, used_words):
-    if type(used_words) is not list:
-        used_words = []
-    from random import choice
-    user_word = choice(read_answers_data("data/words")[last_char])
-    while user_word in used_words:
-        user_word = choice(read_answers_data("data/words")[last_char])
-    return user_word
-
-
-# Ну вот эта функция всем функциям функция, ага. Замена постоянному формированию ответа, ага, экономит 4 строчки!!
-def message_return(response, user_storage, message):  # ща будет магия
-    response.set_text(message)
-    response.set_tts(message)
-    buttons, user_storage = get_suggests(user_storage)
-    response.set_buttons(buttons)
-    return response, user_storage
-
 
 def handle_dialog(request, response, user_storage, database):
     from random import choice
     if not user_storage:
         user_storage = {"suggests": []}
     input_message = request.command.lower()
-    user_word = input_message.split()
     if request.is_new_session:
         if not database.get_entry(request.user_id):
             output_message = "Привет-привет. Не хочешь ли ты сыграть в слова? Если да, то просто назови своё имя!"
@@ -39,6 +17,16 @@ def handle_dialog(request, response, user_storage, database):
                 "Продолжить",
                 "Начать сначала"
             ]}
+        return message_return(response, user_storage, output_message)
+
+    if "помощь" in input_message or "что ты умеешь" in input_message:
+        output_message = "В этой игре тебе просто-напросто нужно говорить слово, начинающиеся с той же буквы, " \
+                         "что заканчивается предыдущее. Статистика отгаданных слов сохраняется, при начале новой игры" \
+                         " счет в данный момент обнуляется, а максимальный остается. Что ж, сыграем?"
+        user_storage = {'suggests': [
+            "Продолжить",
+            "Начать сначала"
+        ]}
         return message_return(response, user_storage, output_message)
 
     if input_message in ['не хочется', 'в следующий раз', 'выход', "не хочу", 'выйти']:
@@ -56,8 +44,6 @@ def handle_dialog(request, response, user_storage, database):
 
     if "таблица лидеров" in input_message or "лидеры" in input_message or "рейтинг" in input_message:
         leaders = database.make_leaderboard()
-        # Получение таблицы лидеров, нужно получать лист, как снизу, да, именно так.
-        # leaders = [(1, "Дима бох", 1487), (99999999, "Гоша лох", 0)]
         output_message = "Имеющиеся на данный момент лидеры:\n{}\n" \
             .format(",\n".join(["{}. {}, счет - {}".format(i[0], i[1].capitalize(), i[2]) for i in leaders]))
         current_user_stats = database.get_entry(request.user_id)[0]
@@ -75,12 +61,11 @@ def handle_dialog(request, response, user_storage, database):
         return message_return(response, user_storage, output_message)
 
     entry = database.get_entry(request.user_id)[0]
-    data = (entry[1], entry[2], database.get_words(request.user_id))
     if "продолжить" in input_message or "давай" in input_message or "сначала" in input_message or (
-            "хочу" in input_message and "не" in input_message) and not data[1]:
-        chosen_word = choice_wrd(data[1] if data[1] else choice("айцукенгшщзхфывапролджэячсмитбю"), data[2])
+            "хочу" in input_message and "не" in input_message) and not entry[2]:
+        chosen_word = choice_wrd(entry[2] if entry[2] else choice("айцукенгшщзхфывапролджэячсмитбю"),
+                                 database.get_words(request.user_id))
         if "сначала" in input_message:
-            print(data[2])
             database.update(request.user_id, chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
                             current_score=0, max_score=entry[3] if entry[3] > entry[4] else entry[4])
         else:
@@ -91,15 +76,16 @@ def handle_dialog(request, response, user_storage, database):
         user_storage = {'suggests': []}
         return message_return(response, user_storage, output_message)
 
-    elif data and data[1]:
-        if user_word[0][0].lower() == data[1]:
+    elif entry and entry[2]:
+        user_word = input_message.split()
+        if user_word[0][0].lower() == entry[2]:
             if check_wrd(user_word[0]):
-                used_words = data[2]
+                used_words = database.get_words(request.user_id)
                 if user_word[0] not in used_words:
                     used_words.append(user_word[0])
                     chosen_word = choice_wrd(user_word[0][-1] if user_word[0][-1] not in "ьъ" else user_word[0][-2],
                                              used_words)
-                    output_message = "Правильно! Следующее слово - {}".format(chosen_word)
+                    output_message = "Правильно! Следующее слово - {}.".format(chosen_word)
                     database.update(request.user_id,
                                     chosen_word[-1] if chosen_word[-1] not in "ьъ" else chosen_word[-2],
                                     current_score=entry[3] + 1, max_score=entry[3] if entry[3] > entry[4] else entry[4])
@@ -107,11 +93,11 @@ def handle_dialog(request, response, user_storage, database):
                 else:
                     output_message = "Неправильно, это слово уже использовалось!"
             else:
-                output_message = "Неправильно, ты это слово выдумал, что ли?"
+                output_message = "Неправильно, ты это слово выдумал, что ли? Если нет, то скорее всего его просто нет в моем словаре, лучше подбери другой вариант."
         else:
-            output_message = "Неправильно! Слово начинается не с той буквы, напоминаю, должна быть буква {}" \
-                .format(data[1])
+            output_message = "Неправильно! Слово начинается не с той буквы, напоминаю, должна быть буква {}." \
+                .format(entry[2])
         return message_return(response, user_storage, output_message)
 
     buttons, user_storage = get_suggests(user_storage)
-    return IDontUnderstand(response, user_storage, aliceAnswers["cantTranslate"])
+    return message_error(response, user_storage, aliceAnswers["cantTranslate"])
